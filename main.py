c0013dbf6bfe77a4861e1db9bf6cfc7c0597bbbf
Ero-Emma <78004600+Ero-Emma@users.noreply.github.com>
2021-02-06 20:10:31 -0800

    file

---
+++ main.py
@@ -0,0 +1,253 @@
+{
+  "passages": [
+    {
+      "text": "You have has been sent to the Red Forest to look for items for your mothers stew, how quaint. You need four things.\n''1.) radishes \n2.) onions \n3.) potatos\n4.) some kind of meat ''\nlets hope you can get these things before sunset.\n\n[[Go look for radishes-> Deep Forest]]\n[[Go look for onions->Grass Lands]]\n[[Go look for potatos->Old farm]]\n[[Go hunting->hunting grounds]]",
+      "links": [
+        {
+          "name": "Go look for radishes",
+          "link": " Deep Forest",
+          "pid": "2"
+        },
+        {
+          "name": "Go look for onions",
+          "link": "Grass Lands",
+          "pid": "3"
+        },
+        {
+          "name": "Go look for potatos",
+          "link": "Old farm",
+          "pid": "4"
+        },
+        {
+          "name": "Go hunting",
+          "link": "hunting grounds",
+          "pid": "5"
+        }
+      ],
+      "name": "The Red Forest",
+      "pid": "1",
+      "position": {
+        "x": "404",
+        "y": "170.5"
+      }
+    },
+    {
+      "text": "you look around and find the radishes fairly easily. No suprise, no one even likes these things.\n\n[[Go look for onions->Grass Lands]]\n[[Go look for potatos->Old farm]]\n[[Go hunting->hunting grounds]]",
+      "links": [
+        {
+          "name": "Go look for onions",
+          "link": "Grass Lands",
+          "pid": "3"
+        },
+        {
+          "name": "Go look for potatos",
+          "link": "Old farm",
+          "pid": "4"
+        },
+        {
+          "name": "Go hunting",
+          "link": "hunting grounds",
+          "pid": "5"
+        }
+      ],
+      "name": " Deep Forest",
+      "pid": "2",
+      "position": {
+        "x": "209",
+        "y": "320.5"
+      }
+    },
+    {
+      "text": "You scrounge around for onions, but all you find is onion grass. That will have to work.\n\n[[Go look for radishes-> Deep Forest]]\n[[Go look for potatos->Old farm]]\n[[Go hunting->hunting grounds]]",
+      "links": [
+        {
+          "name": "Go look for radishes",
+          "link": " Deep Forest",
+          "pid": "2"
+        },
+        {
+          "name": "Go look for potatos",
+          "link": "Old farm",
+          "pid": "4"
+        },
+        {
+          "name": "Go hunting",
+          "link": "hunting grounds",
+          "pid": "5"
+        }
+      ],
+      "name": "Grass Lands",
+      "pid": "3",
+      "position": {
+        "x": "359",
+        "y": "320.5"
+      }
+    },
+    {
+      "text": "You dig your hands through the dirt and pick out a few small potatos, yay!\n\n[[Go look for radishes-> Deep Forest]]\n[[Go look for onions->Grass Lands]]\n[[Go hunting->hunting grounds]]",
+      "links": [
+        {
+          "name": "Go look for radishes",
+          "link": " Deep Forest",
+          "pid": "2"
+        },
+        {
+          "name": "Go look for onions",
+          "link": "Grass Lands",
+          "pid": "3"
+        },
+        {
+          "name": "Go hunting",
+          "link": "hunting grounds",
+          "pid": "5"
+        }
+      ],
+      "name": "Old farm",
+      "pid": "4",
+      "position": {
+        "x": "509",
+        "y": "320.5"
+      }
+    },
+    {
+      "text": "You arive at the hunting ground. You see a trap and a hunting rifle. you can use either one.\n\n[[use the rifle-> boar]]\n[[use the trap-> rabit]]\n\n[[Go look for radishes-> Deep Forest]]\n[[Go look for onions->Grass Lands]]\n[[Go look for potatos->Old farm]]\n",
+      "links": [
+        {
+          "name": "use the rifle",
+          "link": " boar",
+          "pid": "6"
+        },
+        {
+          "name": "use the trap",
+          "link": " rabit",
+          "pid": "7"
+        },
+        {
+          "name": "Go look for radishes",
+          "link": " Deep Forest",
+          "pid": "2"
+        },
+        {
+          "name": "Go look for onions",
+          "link": "Grass Lands",
+          "pid": "3"
+        },
+        {
+          "name": "Go look for potatos",
+          "link": "Old farm",
+          "pid": "4"
+        }
+      ],
+      "name": "hunting grounds",
+      "pid": "5",
+      "position": {
+        "x": "647",
+        "y": "320.5"
+      }
+    },
+    {
+      "text": "(set: $holding to \"rifel\")\n\nyou dicide to get to get something a little more filling, so you grab the gun and set out deeper into the forest. \nyou see a russling in the grass\n\n[[wait-> catch]]\n[[fire!!-> Runs]]",
+      "links": [
+        {
+          "name": "wait",
+          "link": " catch",
+          "pid": "8"
+        },
+        {
+          "name": "fire!!",
+          "link": " Runs",
+          "pid": "9"
+        }
+      ],
+      "name": " boar",
+      "pid": "6",
+      "position": {
+        "x": "572",
+        "y": "470.5"
+      }
+    },
+    {
+      "text": "You set the trap and wait for a bit. you spend a while trying to look at your phone but the signal out here is trash so you end up just standing around.\n\nafter two hours you decide to check on your trap and see a rabit. You aren't looking forward to killing it. :(\n\n[[Go look for radishes-> Deep Forest]]\n[[Go look for onions->Grass Lands]]\n[[Go look for potatos->Old farm]]\n",
+      "links": [
+        {
+          "name": "Go look for radishes",
+          "link": " Deep Forest",
+          "pid": "2"
+        },
+        {
+          "name": "Go look for onions",
+          "link": "Grass Lands",
+          "pid": "3"
+        },
+        {
+          "name": "Go look for potatos",
+          "link": "Old farm",
+          "pid": "4"
+        }
+      ],
+      "name": " rabit",
+      "pid": "7",
+      "position": {
+        "x": "722",
+        "y": "470.5"
+      }
+    },
+    {
+      "text": " you wait for a moment...\n \n the boar looks up, and you get the perfect shot!\n \n [[Go look for radishes-> Deep Forest]]\n[[Go look for onions->Grass Lands]]\n[[Go look for potatos->Old farm]]\n",
+      "links": [
+        {
+          "name": "Go look for radishes",
+          "link": " Deep Forest",
+          "pid": "2"
+        },
+        {
+          "name": "Go look for onions",
+          "link": "Grass Lands",
+          "pid": "3"
+        },
+        {
+          "name": "Go look for potatos",
+          "link": "Old farm",
+          "pid": "4"
+        }
+      ],
+      "name": " catch",
+      "pid": "8",
+      "position": {
+        "x": "497",
+        "y": "620.5"
+      }
+    },
+    {
+      "text": "You fire recklessly and the boar runs away. Oopsie :(\n\n[[Go look for radishes-> Deep Forest]]\n[[Go look for onions->Grass Lands]]\n[[Go look for potatos->Old farm]]\n",
+      "links": [
+        {
+          "name": "Go look for radishes",
+          "link": " Deep Forest",
+          "pid": "2"
+        },
+        {
+          "name": "Go look for onions",
+          "link": "Grass Lands",
+          "pid": "3"
+        },
+        {
+          "name": "Go look for potatos",
+          "link": "Old farm",
+          "pid": "4"
+        }
+      ],
+      "name": " Runs",
+      "pid": "9",
+      "position": {
+        "x": "647",
+        "y": "620.5"
+      }
+    }
+  ],
+  "name": "project 1",
+  "startnode": "1",
+  "creator": "Twine",
+  "creator-version": "2.3.12",
+  "ifid": "A4C80C3F-99D7-4804-BACF-44D8810AA878"
+}
